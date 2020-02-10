b2802264704d48a4e341a8429f9fc1453155f265
asphaltexistenzler <simon.zmugg@outlook.com>
2020-02-10 20:30:06 +0100

    gitadd

---
+++ test.py
@@ -0,0 +1,20 @@
+print('the boy was flattered,\nnever ever has he been given such a great gift,')
+print('never ever has anyone been so generous to him.')
+print("He stuttered:\"m... ma... mark, please, i\'ll delete your data on the subject, it will be as if it would\'ve never happened.")
+print("\"accept this small gesture of me, \as for now I cant do anything, except for this\" he continued")
+
+print(10+10)
+print((20*57)-(-70)**3)
+print(9/3)
+print(8%3)
+print(10**20)
+
+from fractions import Fraction
+bruch1=Fraction(1,2)
+bruch2=Fraction(5,6)
+print (bruch1/bruch2)
+
+erg = bruch1+2*bruch2
+divmod (erg.numerator, erg.denominator)
+(2,1)
+print (erg)
